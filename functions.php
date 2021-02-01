16196092b09d470cab37062472a3a44c0f215e3c
Jordan Taylor <jordantaylor384@gmail.com>
2021-02-01 09:51:43 +0000

    added admin logo to login

--- functions.php
+++ functions.php
@@ -164,7 +164,7 @@ add_action('admin_enqueue_scripts', 'admin_theme_style');
 function my_login_logo() { ?>
 	<style type="text/css">
 	#login h1 a, .login h1 a {
-		background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo-black.png);
+		background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/anne_archer_small_logo2x.png);
 		height: 65px;
 		width: 320px;
 		/* background-size: 320px 65px; */
