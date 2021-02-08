cdd2c26f56a6c87f4dd7244ea16b5616d4918eaf
Jordan Taylor <jordantaylor384@gmail.com>
2021-02-08 15:03:29 +0000

    added cookie status shortcode

--- functions.php
+++ functions.php
@@ -455,3 +455,19 @@ function filter_ptags_on_images($content){
     return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 add_filter('the_content', 'filter_ptags_on_images');
+
+//------------------------------------------------------------------------------
+
+// function that runs when shortcode is called
+function cookie_status() {
+
+ if (cn_cookies_accepted()) {
+	 $message = '<p>Consent Accepted</p>';
+ } else {
+	 $message = '<p>Consent Refused</p>';
+ }
+
+return $message;
+}
+// register shortcode
+add_shortcode('cookie_status', 'cookie_status');
