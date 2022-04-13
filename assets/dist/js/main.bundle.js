36422d3553aa320a97370485f86a1f9f678ac55e
Jordan Taylor <jordantaylor384@gmail.com>
2022-04-13 16:27:53 +0100

    dev: rich amends

--- assets/dist/js/main.bundle.js
+++ assets/dist/js/main.bundle.js
@@ -476,10 +476,16 @@ function init() {
 
   $('.sticky-nav .nav-toggle').on('click', function () {
     $('.overlay').toggleClass('show');
-  }); // // close sticky nav
-  // $('.overlay__close').on('click', function() {
-  //   $('.overlay').toggleClass('show');
-  // });
+  }); // toggle parent on overlay nav item
+
+  $('.overlay__nav .menu li.menu-item-has-children').on('click', function () {
+    $(this).find('.sub-menu-container').slideToggle();
+    $(this).toggleClass('open');
+  }); // check if current page is a child of a parent.
+  // if so open the nav on load
+
+  $('.overlay__nav .menu li.current_page_parent').find('.sub-menu-container').slideToggle();
+  $('.overlay__nav .menu li.current_page_parent').toggleClass('open');
 }
 
 /***/ }),
--- assets/src/js/overlay.js
+++ assets/src/js/overlay.js
@@ -7,14 +7,21 @@ export function init() {
   $('.overlay__close').on('click', function() {
     $('.overlay').toggleClass('show');
   });
-
-
   // toggle sticky nav
   $('.sticky-nav .nav-toggle').on('click', function() {
     $('.overlay').toggleClass('show');
   });
-  // // close sticky nav
-  // $('.overlay__close').on('click', function() {
-  //   $('.overlay').toggleClass('show');
-  // });
+
+
+  // toggle parent on overlay nav item
+  $('.overlay__nav .menu li.menu-item-has-children').on('click', function() {
+    $(this).find('.sub-menu-container').slideToggle();
+    $(this).toggleClass('open');
+  });
+
+  // check if current page is a child of a parent.
+  // if so open the nav on load
+  $('.overlay__nav .menu li.current_page_parent').find('.sub-menu-container').slideToggle();
+  $('.overlay__nav .menu li.current_page_parent').toggleClass('open');
+
 }
